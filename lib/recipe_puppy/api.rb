76fbc70f83d28665d0da59ebb996fee9478e7e8d
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-03-30 00:19:05 +0000

    Created response variable and parsed the response from the API into a hash

--- lib/recipe_puppy/api.rb
+++ lib/recipe_puppy/api.rb
@@ -5,6 +5,8 @@ class API
   def self.get_recipes 
     url = "http://www.recipepuppy.com/api/?q=puertorican"
     uri = URI(url)
+    response = Net::HTTP.get(uri)
+    parsed_hash = JSON.parse(response)
   end 
 end 
 
