37a05c7b026b6a48d31c2c98d963b13f9ae1a125
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-03-30 01:11:48 +0000

    Required relative files inside the recipe_puppy.rb file

--- lib/recipe_puppy.rb
+++ lib/recipe_puppy.rb
@@ -1,6 +1,15 @@
-require "recipe_puppy/version"
+require_relative "./recipe_puppy/version"
+require_relative './recipe_puppy/api.rb'
+require_relative './recipe_puppy/cli.rb'
+require_relative './recipe_puppy/recipe.rb'
 
-module RecipePuppy
-  class Error < StandardError; end
+
+
+
+
+
+
+#module RecipePuppy
+  #class Error < StandardError; end
   # Your code goes here...
-end
+#end
