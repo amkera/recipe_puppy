a75f10921b44cc90b4be8ab55f1d6e41a97bcbf3
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-03-30 00:22:16 +0000

    Iterate over results array and instantiate each new recipe

--- lib/recipe_puppy/api.rb
+++ lib/recipe_puppy/api.rb
@@ -7,6 +7,9 @@ class API
     uri = URI(url)
     response = Net::HTTP.get(uri)
     parsed_hash = JSON.parse(response)
-  end 
-end 
-
+    results_array = parsed_hash['results']
+    results_array.each do |recipe| #each recipe is a hash
+      Recipe.new(recipe)
+    end
+  end
+end
