1122100edc348ecc1ed558c1fc5a025647705444
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-03-30 01:17:57 +0000

    Moved required gems to the top of the file

--- lib/recipe_puppy.rb
+++ lib/recipe_puppy.rb
@@ -1,13 +1,15 @@
+require 'HTTParty'
+require 'pry'
+require 'uri'
+require 'json'
+
 require_relative "./recipe_puppy/version"
 require_relative './recipe_puppy/api.rb'
 require_relative './recipe_puppy/cli.rb'
 require_relative './recipe_puppy/recipe.rb'
 
 
-require 'HTTParty'
-require 'pry'
-require 'uri'
-require 'json'
+
 
 
 
